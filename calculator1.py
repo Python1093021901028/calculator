357256deea6cc6820e762e0b25e62dec8f83c961
Python1093021901028 <86838656+Python1093021901028@users.noreply.github.com>
2021-07-02 13:20:39 +0000

    Add files via upload

---
+++ calculator1.py
@@ -0,0 +1,21 @@
+what = input("what we will do (+, -, *, /, **, %) : ")
+a = int(input("first number : "))
+b = int(input("second number : "))
+if what == "+":
+	c = a + b
+	print(c)
+if what == "-":
+	c = a - b
+	print(c)
+if what == "*":
+	c = a * b
+	print(c)
+if what == "/":
+	c = a / b
+	print(c)
+if what == "**":
+	c = a ** b
+	print(c)
+if what == "%":
+	c = a % b
+	print(c)
